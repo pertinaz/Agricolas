dae42915b82b77b572a1b5ccb176e247f7877497
Agudelo <87156289+pertinaz@users.noreply.github.com>
2024-04-23 13:53:22 -0500

    Update main.py

--- Agricolas/main.py
+++ Agricolas/main.py
@@ -4,6 +4,10 @@ from Modelo.productos.productoControl import ProductoControl, ControlPlagas, Con
 from Modelo.clientes.cliente import Cliente
 from Modelo.pedidos.factura import Factura
 
+productos = []
+clientes = []
+pedidos = []
+
 def menu():
     print("\nMENU\n")
     print("1. Crear nuevo producto de control")
@@ -12,61 +16,100 @@ def menu():
     print("4. Realizar nuevo pedido")
     print("5.Salir")
 
-productos = []
-clientes = []
-pedidos = []
+def listaProductos():
+    print("\nLISTA DE PRODUCTOS")
+    n=0
+    for producto in productos:
+        for nombreProducto in producto:     
+            print(f"{n+1}.{nombreProducto}")
+            disponibles = producto.count()
+            print(f"Cantidad de unidades disponibles: {disponibles}")
+
+def crearProductoControl():
+    nombre = input("Ingrese el nombre del producto de control: ")
+    registroICA = input("Ingrese el registro ICA del producto: ")
+    frecuenciaAplicacion = input("Ingrese la frecuencia de aplicación del producto: ")
+    valorProducto = float(input("Ingrese el valor del producto: "))
+    disponibleInventario = int(input("Ingrese la cantidad de unidades: "))
+    tipoProducto = input("Ingrese el tipo de producto (Control de plagas/Fertilizantes): ").lower()
+
+    if tipoProducto == "plaga":
+        periodoCarencia = int(input("Ingrese el periodo de carencia del producto (en dias): "))
+        nuevoProducto = ControlPlagas(registroICA,nombre,frecuenciaAplicacion,valorProducto,disponibleInventario,periodoCarencia
+        )
+    
+    elif tipoProducto == "fertilizante":
+        ultimaAplicacion = input("Ingrese la fecha de la última aplicación del producto (YYYY-MM-DD): ")
+        nuevoProducto = ControlFertilizantes(registroICA,nombre,frecuenciaAplicacion,valorProducto,disponibleInventario,ultimaAplicacion
+        )
+    else:
+        print("Tipo de producto no válido.")
+    productos.append(nuevoProducto)
+    print("Producto creado con éxito.")
+
+def crearNuevoAntibiotico():
+    nombre = input("Ingrese el nombre del antibiótico: ")
+    dosis = float(input("Ingrese la dosis del antibiótico (entre 400 y 600 Kg): "))
+    tipoAnimal = input("Ingrese el tipo de animal al que se puede aplicar (Bovinos/Caprinos/Porcinos): ")
+    valorProducto = float(input("Ingrese el precio del antibiótico: "))
+    disponibleInventario = int(input("Ingrese la cantidad de unidades: "))
+    nuevoAntibiotico = Antibiotico(nombre,dosis,tipoAnimal,valorProducto,disponibleInventario)
+    productos.append(nuevoAntibiotico)
+    print("Antibiótico creado con éxito")
+
+def crearNuevoCliente():
+    nombre = input("Ingrese el nombre del cliente: ")
+    cedula = input("Ingrese la cedula del cliente: ")
+    nuevoCliente = Cliente(nombre,cedula)
+    clientes.append(nuevoCliente)
+    print("Cliente creado con éxito")
+
+def realizarNuevoPedido():
+    if len(clientes) == 0:
+        print("Debe crear al cliente antes de realizar el pedido.")
+    cliente = None
+    while cliente is None:
+        cedulaCliente = input("Ingrese la cédula del cliente: ")
+        cliente = next((cliente for cliente in clientes if cliente.cedula == cedulaCliente),None)
+        if cliente is None:
+            print("Cliente no encontrado. Intente nuevamente.")
+        
+    # Agregar productos al pedido
+    procesoCompra = True
+    subtotal = 0.0
+    while procesoCompra:
+        listaProductos()
+        seleccionarProducto = input("Ingrese el nombre del producto que desea agregar al carrito de compra: ")
+        seleccionarCantidad = int(input("Ingrese cantidad de unidades que desea agregar al carrito de compra: "))
+        
+        for producto in productos:
+            if seleccionarProducto == producto.nombreProducto:
+                subtotal += producto.valorProducto * seleccionarCantidad
+                break
+        print(f"El subtotal de su compra es: {subtotal}\n")
+        auxAgregar = input("1. continuar agregando productos al carrito.\n2. finalizar compra.")
+        if auxAgregar == '2':
+            fecha = "2024-04-20"
+            total = subtotal
+            nuevoPedido = Factura(clientes.nombre,seleccionarProducto,fecha,total)
+            pedidos.append(nuevoPedido)
+
+            procesoCompra = False # Finalizar proceso de compra
+        else:
+            print("Ingrese una opción válida.")
+    print("Pedido realizado con éxito.")
 
 while True:
     menu()
     option = input("Seleccione una opción: ")
-
     if option == "1":
-        nombre = input("Ingrese el nombre del producto de control: ")
-        registroICA = input("Ingrese el registro ICA del producto: ")
-        frecuenciaAplicacion = input("Ingrese la frecuencia de aplicación del producto: ")
-        valorProducto = float(input("Ingrese el valor del producto: "))
-        tipoProducto = input("Ingrese el tipo de producto (Control de plagas/Fertilizantes): ").lower()
-
-        if tipoProducto == "plaga":
-            periodoCarencia = int(input("Ingrese el periodo de carencia del producto (en dias): "))
-            nuevoProducto = ControlPlagas(registroICA,nombre,frecuenciaAplicacion,valorProducto,periodoCarencia
-            )
-        
-        if tipoProducto == "fertilizante":
-            ultimaAplicacion = input("Ingrese la fecha de la última aplicación del producto (YYYY-MM-DD): ")
-            nuevoProducto = ControlFertilizantes(registroICA,nombre,frecuenciaAplicacion,valorProducto,ultimaAplicacion
-            )
-        else:
-            print("Tipo de producto no válido.")
-            continue
-        productos.append(nuevoProducto)
-        print("Producto creado con éxito.")
+        crearProductoControl()
     elif option == "2":
-        nombre = input("Ingrese el nombre del antibiótico: ")
-        dosis = float(input("Ingrese la dosis del antibiótico (entre 400 y 600 Kg): "))
-        tipoAnimal = input("Ingrese el tipo de animal al que se puede aplicar (Bovinos/Caprinos/Porcinos): ")
-        precio = float(input("Ingrese el precio del antibiótico: "))
-        nuevoAntibiotico = Antibiotico(nombre,dosis,tipoAnimal,precio)
-        productos.append(nuevoAntibiotico)
-        print("Antibiótico creado con éxito")
+        crearNuevoAntibiotico()
     elif option == "3":
-        nombre = input("Ingrese el nombre del cliente: ")
-        cedula = input("Ingrese la cedula del cliente: ")
-        nuevoCliente = Cliente(nombre,cedula)
-        clientes.append(nuevoCliente)
-        print("Cliente creado con éxito")
+        crearNuevoCliente()
     elif option == "4":
-        if len(clientes) == 0:
-            print("Crear al cliente antes de realizar el pedido.")
-            continue
-        cliente = None
-        while cliente is None:
-            cedulaCliente = input("Ingrese la cédula del cliente: ")
-            cliente = next((cliente for cliente in clientes if cliente.cedula == cedulaCliente),None)
-            if cliente is None:
-                print("Cliente no encontrado. Intente nuevamente.")
-        
-        print("Pedido realizado con éxito.")
+        realizarNuevoPedido()
     elif option == "5":
         print("Sistema cerrado.")
         break
