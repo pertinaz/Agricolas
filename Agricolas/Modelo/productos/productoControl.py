0b695e3888f08d775da53375ac7042de58ac8a9c
Agudelo <87156289+pertinaz@users.noreply.github.com>
2024-04-25 17:50:57 -0500

    Update productoControl.py

--- Agricolas/Modelo/productos/productoControl.py
+++ Agricolas/Modelo/productos/productoControl.py
@@ -1,24 +1,52 @@
 class ProductoControl:
-    def __init__(self,registroICA,nombreProducto,frecuenciaAplicacion,valorProducto):
+    def __init__(self,registroICA,nombreProducto,frecuenciaAplicacion,valorProducto,inventario):
         self.registroICA = registroICA
         self.nombreProducto = nombreProducto
         self.frecuenciaAplicacion = frecuenciaAplicacion # (es decir, cada cuanto periodo se aplica el producto.Cada 15 días, cada 30 días, etc)
         self.valorProducto = valorProducto
+        self.inventario = inventario
 
+        self.__productosDisponibles = []
+
+    @property
+    def productosDisponibles(self):
+        return self.__productosDisponibles
+    
+    @productosDisponibles.setter
+    def productosDisponibles(self,producto):
+        self.__productosDisponibles.append(producto)
+
+    def registrarProducto(self,producto):
+        self.__productosDisponibles = producto
+        
 class ControlPlagas(ProductoControl):
     def __init__(self,registroICA
-    ,nombreProducto,frecuenciaAplicacion,valorProducto,periodoCarencia):
-        super().__init__(registroICA,nombreProducto,frecuenciaAplicacion, valorProducto)
+    ,nombreProducto,frecuenciaAplicacion,valorProducto,inventario,periodoCarencia):
+        super().__init__(registroICA,nombreProducto,frecuenciaAplicacion, valorProducto,inventario)
         self.periodoCarencia = periodoCarencia # (es el tiempo legalmenteestablecido, expresado usualmente en número de días que debe transcurrir entre la última aplicación de un fitosanitario y la cosecha)
 
 class ControlFertilizantes(ProductoControl):
-    def __init__(self,registroICA,nombreProducto,frecuenciaAplicacion,valorProducto,fechaUltimaAplicacion):
-        super().__init__(registroICA,nombreProducto,frecuenciaAplicacion,valorProducto)
+    def __init__(self,registroICA,nombreProducto,frecuenciaAplicacion,valorProducto,inventario,fechaUltimaAplicacion):
+        super().__init__(registroICA,nombreProducto,frecuenciaAplicacion,valorProducto,inventario)
         self.fechaUltimaAplicacion = fechaUltimaAplicacion
 
 class Antibiotico:
-    def __init__(self,nombreProducto,dosis,tipoAnimal,precio):
+    def __init__(self,nombreProducto,dosis,tipoAnimal,precio,inventario):
         self.nombreProducto = nombreProducto
         self.dosis = dosis
         self.tipoAnimal = tipoAnimal
         self.precio = precio
+        self.inventario = inventario
+
+        self.__antibioticos = []
+        
+    @property
+    def antibioticos(self):
+        return self.__antibioticosDisponibles
+    
+    @antibioticos.setter
+    def antibioticos(self,producto):
+        self.__antibioticosDisponibles.append(producto)
+
+    def registrarAntibiotico(self,producto):
+        self.__antibioticosDisponibles = producto
