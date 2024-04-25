ba15edc02e2a0289b45bce77dac448195b1294fc
Agudelo <87156289+pertinaz@users.noreply.github.com>
2024-04-25 17:48:09 -0500

    Update cliente.py
    Clients functions update

--- Agricolas/Modelo/clientes/cliente.py
+++ Agricolas/Modelo/clientes/cliente.py
@@ -1,5 +1,23 @@
-
 class Cliente:
     def __init__(self,nombreCliente,cedulaCliente):
-        self.NombreCliente = nombreCliente
+        self.nombreCliente = nombreCliente
         self.cedulaCliente = cedulaCliente
+        self.__clientes = [] # Crear un metodo de clase (base de datos) para guardar los clientes 
+
+    @property
+    def clientes(self):
+        return self.__clientes
+    
+    @clientes.setter
+    def clientes(self,cliente):
+        self.__clientes.append(cliente)
+    
+    def registrarCliente(self,cliente):
+        self.__clientes = cliente
+    
+    def buscarCedula(self,cedulaCliente):
+        for cedula in self.__clientes.cedulaCliente:
+            if cedulaCliente == cedula:
+                return (f"DATOS DEL CLIENTE: \n{self.__clientes}")
+            else: 
+                raise ValueError(f"No existe tal cliente con cedula: {cedulaCliente}")
