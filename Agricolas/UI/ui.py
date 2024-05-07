fbb5c835929f85808dc78c45012d16035edbeb4b
Agudelo <87156289+pertinaz@users.noreply.github.com>
2024-05-07 13:58:05 -0500

    Create ui.py

---
+++ Agricolas/UI/ui.py
@@ -0,0 +1,22 @@
+'''MENUS'''
+def menuGeneral():
+    print("\nMENU\n")
+    print("1. PRODUCTOS")
+    print("2. CLIENTES")
+    print("3. REALIZAR NUEVO PEDIDO")
+    print("4. SALIR")
+
+def menuProductos():
+    print("\nAGRONOMICA DEL NORTE\n")
+    print("1. Crear nuevo producto de control")
+    print("2. Crear nuevo antibiotico")
+    print("3. Borrar producto")
+    print("4. Buscar producto")
+    print("5. Regresar")
+
+def menuClientes():
+    print("\nAGRONOMICA DEL NORTE\n")
+    print("1. Crear nuevo cliente")
+    print("3. Borrar cliente")
+    print("2. Buscar cliente")
+    print("4. Regresar")
